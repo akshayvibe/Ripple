94592daa6374981bd883b6ed7b49148b78b9e4b7
akshayvibe <akshayjha2111@gmail.com>
2026-09-05 00:34:27 +0530

    basic server config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"net/http"
+	"os"
+	"log"
+)
+
 func main() {
-	
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("api is working"))
+	})
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	srv := &http.Server{
+		Addr:    ":"+port,	
+		Handler: mux,
+	}
+	log.Printf("server running on http://localhost:%s", port)
+	if err := srv.ListenAndServe(); err != nil {
+    panic(err)
+	}
 }
